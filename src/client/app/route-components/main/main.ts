151a5b1b013a56991b67c6dfbcd53b411e3e803e
Derek Pavao <derek.pavao@nextsteplivinginc.com>
2015-03-18 09:36:07 -0400

    No JavaScript anymore, its the past :)

---
+++ src/client/app/route-components/main/main.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by dpavao on 3/18/15.
+ */
+
+
+class MainController {
+
+    constructor() {
+        console.log('woot ');
+        debugger;
+    }
+
+}
+
+angular.module('app').controller('MainController', MainController);
